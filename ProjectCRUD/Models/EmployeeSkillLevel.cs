37012304d8e776c8ef0ccb1e32ee7f31fb1d3259
Onurcan Onder <onurcan.onder.dev@gmail.com>
2023-01-12 10:15:09 +0000

    Code cleaning.

--- ProjectCRUD/Models/EmployeeSkillLevel.cs
+++ ProjectCRUD/Models/EmployeeSkillLevel.cs
@@ -10,6 +10,5 @@ namespace ProjectCRUD.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        // public List<Employee>? Employees { get; set; }
     }
 }
